8a19323f8a931146114ace527e391d0be2b4d009
stodipo <odipo87@gmail.com>
2018-07-13 12:31:20 +0300

    Included the js comments

--- js/greetings.js
+++ js/greetings.js
@@ -1,3 +1,5 @@
+/*This script is used to send a greeting to the user based on the
+system clock.*/
 var today=new Date();
 var hourNow=today.getHours();
 var greetings;
@@ -7,8 +9,9 @@ if (hourNow>18){
 }else if(hourNow>12){
 	greeting='Good Afternoon!';
 }else if(hourNow>0){
-	greeting='Good Morning';
+	greeting='Good Morning!';
 } else {
 	greeting='Welcome!';
 }
 document.write('<h3>'+ greeting +'</h3>');
+//credits to JavaScript and jQuery book
